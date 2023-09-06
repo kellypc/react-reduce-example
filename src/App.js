b0970c6070def09f2fc050a007a4e9971cf4e132
Kelly Cruz <kellycruz.ti@gmail.com>
2023-09-06 16:40:18 -0300

    Conecting page to actions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { connect } from "react-redux";
+import { startAction } from './actions/startAction';
+import { stopAction } from './actions/stopAction';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img 
+        src={logo} 
+        className={
+          "App-logo" + 
+          (this.props.rotating ? "":" App-logo-paused")
+        } 
+        alt="logo" 
+        onClick={
+          this.props.rotating ? 
+            this.props.stopAction : this.props.startAction
+        }
+      />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -22,4 +36,13 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = state => ({
+  ...state
+});
+
+const mapDispatchToProps = dispatch => ({
+  startAction: () => dispatch(startAction),
+  stopAction: () => dispatch(stopAction)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
